03e03a680d38030c7f707e61bd3bf0833eb6df75
szfyf <2322686371@qq.com>
2023-04-02 15:37:00 +0800

    基础搭建vite+ts

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="frame">
+    基础构建测试
+  </div>
+</template>
+
+<style scoped>
+</style>
